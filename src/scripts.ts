08911cfee6fada3ba0704137f28916fe73787076
Harrisgrade <56458322+Harrisgrade@users.noreply.github.com>
2022-03-18 16:05:01 -0500

    created-unordered-list

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,6 +1,12 @@
-/* Write Your Typescript or Javascript Here. */
-document.getElementById("click").addEventListener("click", clickFunction);
+document.getElementById("mainbtn").addEventListener("click", clickFunction);
 
 function clickFunction() {
-    console.log('clicked');
+  const menu = document.getElementById("menu-array");
+  const list = document.createElement("li");
+  const img = document.createElement("img");
+  img.src = "images/b7e.jpg";
+
+  list.appendChild(img);
+  menu.appendChild(list);
+  console.log("click");
 }
